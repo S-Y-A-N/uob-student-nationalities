4a8cc0a95bf2d6ae3b5e3ed795892382d292f1f2
SYAN <syanays.git@gmail.com>
2024-11-26 13:37:39 +0300

    add comments

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nationalities of IT Students at UOB
+This is the 2nd assignment for the course ITCS333.
+In this assignment, we fetch data from an API, which represent information of the number of students from different nationalities at IT college of UOB for every year and semester (2018-2023).
--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
 
+// URL of the dataset from the API
 $url = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
+
+// Fetch the data as a json string, and check for error
 $response = file_get_contents($url);
+if ($respone === false) exit('Error fetching data from API');
+
+// Convert the json into an associative array, and check for error
 $result = json_decode($response, true);
+if ($result === null) exit('Error converting JSON to array');
+
+// The data we need is in the 'results' key, so we save it as the result (data) to be used
 $result = $result['results'];
 
 ?>
 
+
+<!-- HTML to present the fetched data -->
 <html lang="en">
 
 <head>
@@ -33,6 +44,7 @@ $result = $result['results'];
         </tr>
       </thead>
       <tbody>
+        <!-- Loop over all the data to show them as HTML table rows -->
         <?php foreach ($result as $data) : ?>
           <tr>
             <td><?= $data['year'] ?></td>
